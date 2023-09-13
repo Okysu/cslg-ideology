5b7f5d7540556e2dfe43bb5cf2149f0e372c78ec
Okysu <yby@ecanse.com>
2023-09-13 20:47:44 +0800

    添加pinia及其持久化

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import './assets/main.css'
+import { createPinia } from 'pinia'
+
+// piniapersistedstate
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
 
 app.use(router)
 
